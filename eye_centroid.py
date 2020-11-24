e35b54bb84a91f1c13551a876e5819553258234c
kartik1395 <52433565+kartik1395@users.noreply.github.com>
2020-11-24 16:07:54 +0800

    Update eye_centroid.py

--- eye_centroid.py
+++ eye_centroid.py
@@ -35,97 +35,92 @@ def hough_method(gray, frame):
 
 
 def find_eye(coords, gray, frame):
+    '''Function to detect the pupil and centroid 
+            using HoughCircles method'''
 
-	delta = 20
-	gray_roi = gray[coords[1]:coords[3]
-	    , coords[0]:coords[2]]
+    # Extract the ROI (eye) detected by the haarcascade
+    gray_roi = gray[coords[1]:coords[3], coords[0]:coords[2]]
 
-	if len(gray_roi) == 0:
-		return frame
+    if len(gray_roi) == 0:
+        return frame
 
-	circles = cv2.HoughCircles(gray_roi, cv2.HOUGH_GRADIENT, 1,
-	                            800, param1=200, param2=9, minRadius=70, maxRadius=100)
-	if circles is None:
-	 return frame
-	 
-	circles = np.uint16(np.around(circles))
-	for i in circles[0, :]:
-	 cv2.circle(frame, (i[0]+coords[0], i[1]+coords[1]), i[2], (0, 255, 0), 2)
-	 cv2.circle(frame, (i[0]+coords[0], i[1]+coords[1]), 3, (0, 0, 255), 1)
+    # Find the circles(pupil) within the ROI
+    circles = cv2.HoughCircles(gray_roi, cv2.HOUGH_GRADIENT, 1,
+                               800, param1=200, param2=9, minRadius=70, maxRadius=100)
+    if circles is None:
+        return frame
 
+    circles = np.uint16(np.around(circles))
+    for i in circles[0, :]:
+        # Draw the pupil and the centroid
+        cv2.circle(frame, (i[0]+coords[0], i[1] +
+                           coords[1]), i[2], (0, 255, 0), 2)
+        cv2.circle(frame, (i[0]+coords[0], i[1]+coords[1]), 3, (0, 0, 255), 1)
 
-	cv2.imshow("Eyes", gray_roi)
-	return frame
+    cv2.imshow("Eyes", gray_roi)
+    return frame
 
 
 def haarcascade_method(gray, frame):
-	eyes=eye_cascade.detectMultiScale(
-	    gray, 1.3, 5, minSize=(100, 100), maxSize=(300, 300))
+    '''Use the opencv available haarcascade for detection of eyes
+            This becomes our ROI for extracting pupil and centroid'''
+    eyes = eye_cascade.detectMultiScale(
+        gray, 1.3, 5, minSize=(100, 100), maxSize=(300, 300))
 
-	for (x, y, w, h) in eyes:
-		cv2.rectangle(frame, (x-20, y-20), (x+w+20, y+h+20), (0, 225, 255), 2)
-		coords=(x-20, y-20, x+w+20, y+h+20)
+    for (x, y, w, h) in eyes:
 
-		final_frame=find_eye(coords, gray, frame)
+        cv2.putText(frame, "Eye", (x-20, y-30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.rectangle(frame, (x-20, y-20), (x+w+20, y+h+20), (0, 225, 255), 2)
+        coords = (x-20, y-20, x+w+20, y+h+20)
 
-	return frame
+        final_frame = find_eye(coords, gray, frame)
+
+    return frame
 
 
 if __name__ == "__main__":
-	
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--video", help='Enter full path of video file', required=True)
-	args = parser.parse_args()
 
-	video_location = args.video
-	cap = cv2.VideoCapture(video_location)
-	eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-	
-	frame_no=1
-	while True:
-		 ret, frame=cap.read()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--video", help='Enter full path of video file', required=True)
+    args = parser.parse_args()
 
-		 if not ret:
-		 	break
+    video_location = args.video
+    cap = cv2.VideoCapture(video_location)
+    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
+    frame_no = 1
+    while True:
+        ret, frame = cap.read()
 
-		 gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        if not ret:
+            break
 
-		 frame=haarcascade_method(gray, frame)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+        frame = haarcascade_method(gray, frame)
 
-		 if not ret:
-		 	print("Frame not recieved")
-		 	break
+        if not ret:
+            print("Frame not recieved")
+            break
 
+        cv2.putText(frame, "Frame count : " + str(frame_no), (20, 40),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 100, 255), 2, cv2.LINE_AA)
 
+        key = cv2.waitKey(1) & 0xff
 
-		 cv2.putText(frame, "Frame count : " + str(frame_no), (20, 40),
-		             cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 100, 255), 2, cv2.LINE_AA)
+        if key == ord('p'):
+            while True:
+                key2 = cv2.waitKey(1) or 0xff
 
-		 key = cv2.waitKey(1) & 0xff
+                if key2 == ord('p'):
+                    break
 
+        cv2.imshow('Eye Tracking', frame)
 
-		 if key == ord('p'):
-		 	while True:
-		 		key2 = cv2.waitKey(1) or 0xff
-		 		
-		 		if key2 == ord('p'):
-		 			break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        frame_no += 1
 
-		 # if key == 27: 
-		 # 	break
-
-
-
-		 cv2.imshow('detected circles1', frame)
-
-
-
-
-		 if cv2.waitKey(1) & 0xFF == ord('q'):
-		 	break
-		 frame_no += 1
-
-	cap.release()
-	cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
